34913e524e241b73496779c7fa7814a91eec9076
xtimlin <xtimlin@gmail.com>
2024-11-05 16:24:20 -0500

    hooks

--- src/booksContext/components/BookList.js
+++ src/booksContext/components/BookList.js
@@ -1,9 +1,8 @@
 import BookShow from "./BookShow";
-import BooksContext from '../context/books'
-import {useContext} from "react";
+import useBooksContext from "../hooks/use-books-context";
 
 function BookList() {
-    const { books } = useContext(BooksContext);
+    const { books } = useBooksContext();
 
     const renderedBooks = books.map((book, index) => {
         return <BookShow key={index} book={book} />;
--- src/booksContext/components/BookShow.js
+++ src/booksContext/components/BookShow.js
@@ -1,10 +1,11 @@
-import {useState, useContext} from 'react';
+import {useState} from 'react';
 import BookEdit from "./BookEdit";
-import BooksContext from '../context/books'
+import useBooksContext from "../hooks/use-books-context";
+
 
 function BookShow({ book }) {
     const [showEdit, setShowEdit] = useState(false);
-    const { deleteBookById } = useContext(BooksContext);
+    const { deleteBookById } = useBooksContext();
 
     const handleDelete = () => { deleteBookById(book.id) };
     const handleEdit = () => { setShowEdit(!showEdit) };
